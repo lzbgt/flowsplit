3d3fa2542037f2af0aa9e7cdc190cf5820d56920
Sergey Chernikov <schernikov@gmail.com>
2014-04-04 20:11:26 -0700

    dependency fix

--- cython/setup.py
+++ cython/setup.py
@@ -4,7 +4,6 @@ Created on Jun 27, 2013
 @author: schernikov
 '''
 
-import numpy as np
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
@@ -23,4 +22,4 @@ ext_modules = cythonize(["nreceiver.pyx"], extra_compile_args=['-DNPY_NO_DEPRECA
 
 res = setup(ext_modules = ext_modules,
             #cmdclass = {'build_ext': cpp_build_ext},
-            script_args=['build_ext', '--inplace'], include_dirs=[np.get_include()])
+            script_args=['build_ext', '--inplace'])
