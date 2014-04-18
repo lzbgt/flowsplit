7c1b3980d6e753bb74eb86172bd27120d70d1b36
Sergey Chernikov <schernikov@gmail.com>
2014-04-17 17:04:07 -0700

    reporting is complete

--- cython/setup.py
+++ cython/setup.py
@@ -3,7 +3,7 @@ Created on Jun 27, 2013
 
 @author: schernikov
 '''
-
+import numpy as np
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
@@ -22,4 +22,4 @@ ext_modules = cythonize(["nreceiver.pyx"], extra_compile_args=['-DNPY_NO_DEPRECA
 
 res = setup(ext_modules = ext_modules,
             #cmdclass = {'build_ext': cpp_build_ext},
-            script_args=['build_ext', '--inplace'])
+            script_args=['build_ext', '--inplace'], include_dirs=[np.get_include()])
